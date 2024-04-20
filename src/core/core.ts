841cc8bfcb3bc10385e4c72c025a551e33e3e06f
Adrian Szuszkiewicz <adrianszuszkiewicz@gmail.com>
2024-04-20 19:07:04 +0200

    Move logging

--- src/core/core.ts
+++ src/core/core.ts
@@ -3,6 +3,7 @@ import { State } from '@/core/state'
 export const increment =
   (by: number) =>
   (state: State): State => {
+    console.log(`core/increment: ${by}`)
     return {
       ...state,
       count: state.count + by
@@ -12,6 +13,7 @@ export const increment =
 export const decrement =
   (by: number) =>
   (state: State): State => {
+    console.log(`core/decrement: ${by}`)
     return {
       ...state,
       count: state.count - by
--- src/shell/shell.ts
+++ src/shell/shell.ts
@@ -13,27 +13,24 @@ export const createShell = (initialState: State) => {
   const thunks = {
     increment: () => {
       console.log(`shell/increment`)
-      console.log(`core/increment: 1`)
       useStore.setState(increment(1))
     },
 
     decrement: () => {
       console.log(`shell/decrement`)
-      console.log(`core/decrement: 1`)
       useStore.setState(decrement(1))
     },
 
     incrementRandom: async () => {
       console.log(`shell/incrementRandom`)
       const number = await randomDelay(5, 10)
-      console.log(`core/increment: ${number}`)
+
       useStore.setState(increment(number))
     },
 
     decrementRandom: async () => {
       console.log(`shell/decrementRandom`)
       const number = await randomDelay(5, 10)
-      console.log(`core/decrement: ${number}`)
       useStore.setState(decrement(number))
     }
   }
